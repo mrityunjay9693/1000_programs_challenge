aca8846088473e43a5d0e592160413244841d28d
mrityunjay9693 <mkaykumar123@gmail.com>
2023-11-24 19:59:06 +0530

    even or odd in python usinf ternary operator

---
+++ Python/evenOdd.py
@@ -0,0 +1,6 @@
+'''
+This is a python program to check whether a given number is odd or not.
+'''
+num = int(input("Enter a number: "))
+
+print(num,"is an even number") if (num % 2 == 0) else print(num,"is an odd number")
