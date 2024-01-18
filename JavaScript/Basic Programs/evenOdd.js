b4ee87e043481ed3500bf9ea55c468eb113b6270
mrityunjay9693 <mkaykumar123@gmail.com>
2024-01-18 20:00:07 +0530

    updates

---
+++ JavaScript/Basic Programs/evenOdd.js
@@ -0,0 +1,9 @@
+/**
+ * This is a JS program to check whether a given number is even or not.
+ */
+let num = 11;
+if (num % 2 == 0) {
+  console.log(num, "is an even number");
+} else {
+    console.log(num,"is a odd number")
+}
---
+++ Python/Basic Programs/sumOfEvenOdd.py
@@ -0,0 +1,18 @@
+'''
+Q2: Write a program in python program to find the sum of odd and even numbers from 1 to N.
+    Problem Description
+    The program takes the number N and finds the sum of odd and even numbers from 1 to N.
+'''
+N = int(input("Enter Value of N: "))
+sumEven = 0
+sumOdd = 0
+
+for i in range(1, N+1):
+    if i % 2 == 0:
+        sumEven = sumEven + i
+    else:
+        sumOdd = sumOdd + i
+
+print("Sum of even numbers from 1 to N:",sumEven)
+print("Sum of odd numbers from 1 to N: ",sumOdd)
+    
